2d4ddbf821f53a32463ecd4d45284f83e6931238
jake@hippiejake.doesntexist.com <jake@hippiejake.doesntexist.com@7359b095-4084-3f36-dd75-96f0e6021f0d>
2010-07-02 05:49:26 +0000

    Most of basic rendering framework seems done. Trying to render testpages/basic.htm. The display tree seems to be getting built correctly, but the text nodes aren't always getting in where they need to be, so the actual rendering process results in nothing. It's not quite there yet, but I feel I'm just one or two "Eureka!" moments away from everything more or less working.

--- trunk/urwid_base/Renderer.py
+++ trunk/urwid_base/Renderer.py
@@ -1,3 +1,4 @@
+import html
 
 class Renderer(object):
 	def __init__(self, topWindow):
@@ -5,4 +6,10 @@ class Renderer(object):
 		self.topWindow = topWindow
 
 	def render(self, dom):
-		pass
+		self.document = dom
+		self.bodyNode = self.document.getElementsByTagName('body')[0]
+		self.bodyDisplayNode = html.SpecialNode_body(self.bodyNode)
+		self.topWindow.widget_list[2] = self.bodyDisplayNode
+		#raise Exception(repr( [x for x in self.bodyDisplayNode.widget_list[2].render((80,)).content()] ))
+		#self.topWindow.contentArea._invalidate()
+		#self.topWindow._invalidate()
--- trunk/urwid_base/TopWindow.py
+++ trunk/urwid_base/TopWindow.py
@@ -4,10 +4,14 @@ import TitleBar, UrlBar
 class TopWindow(urwid.Pile):
 	def __init__(self, browser):
 		self.browser = browser
+
 		self.titlebar = TitleBar.TitleBar()
-		self.contentArea = urwid.Pile((urwid.Text(''),))
 		self.urlbar = UrlBar.UrlBar()
-		super(TopWindow, self).__init__((self.titlebar, self.urlbar, self.contentArea), self.urlbar)
+
+		self.contentArea = urwid.Pile((urwid.Text('halp'),))
+
+		self.contents = [self.titlebar, self.urlbar, self.contentArea]
+		super(TopWindow, self).__init__(self.contents, self.urlbar)
 
 	def keypress(self, size, key):
 		if key not in ('up', 'down'): # Stop Pile from changing focus
